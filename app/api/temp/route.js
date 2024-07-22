0a1480deae277828844c41e77d17847e3b2f92ce
fawad404 <fawadanxari31@gmail.com>
2024-07-22 10:11:36 -0700

    listings-of-resturrants

--- app/api/temp/route.js
+++ app/api/temp/route.js
@@ -1,7 +1,34 @@
+import { restaurants } from "@/app/utilis/db"; 
 import { NextResponse } from "next/server";
 
-export function GET(){
-    
-    console.log("hello world");
-    return NextResponse.json("hello world");
+// Handle GET request
+export  function GET(req) {
+
+  let filteredRestaurants = restaurants;
+
+  return NextResponse.json(filteredRestaurants);
+}
+
+// Handle POST request
+export async function POST(req) {
+  const body = await req.json();
+  const { what, location } = body;
+
+  let filteredRestaurants = restaurants;
+
+  if (what) {
+    filteredRestaurants = filteredRestaurants.filter(restaurant =>
+      restaurant.name.toLowerCase().includes(what.toLowerCase()) ||
+      restaurant.food.toLowerCase().includes(what.toLowerCase()) ||
+      restaurant.service.toLowerCase().includes(what.toLowerCase())
+    );
+  }
+
+  if (location) {
+    filteredRestaurants = filteredRestaurants.filter(restaurant =>
+      restaurant.location.toLowerCase().includes(location.toLowerCase())
+    );
+  }
+
+  return NextResponse.json(filteredRestaurants);
 }
---
+++ app/utilis/db.js
@@ -0,0 +1,45 @@
+export const restaurants = [
+    {
+      id: 1,
+      name: 'Sushi Paradise',
+      food: 'Sushi',
+      service: 'Dine-In',
+      location: 'Tokyo, Japan'
+    },
+    {
+      id: 2,
+      name: 'Pizza Palace',
+      food: 'Pizza',
+      service: 'Takeaway',
+      location: 'New York, USA'
+    },
+    {
+      id: 3,
+      name: 'Curry House',
+      food: 'Indian',
+      service: 'Delivery',
+      location: 'London, UK'
+    },
+    {
+      id: 4,
+      name: 'Burger Barn',
+      food: 'Burgers',
+      service: 'Dine-In',
+      location: 'Sydney, Australia'
+    },
+    {
+      id: 5,
+      name: 'Taco Town',
+      food: 'Tacos',
+      service: 'Dine-In',
+      location: 'Mexico City, Mexico'
+    },
+    {
+      id: 6,
+      name: 'Pizza Palace',
+      food: 'Pizza',
+      service: 'Takeaway',
+      location: 'New York, USA'
+    },
+  ];
+  
