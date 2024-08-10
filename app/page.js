1cba22f7347c0e010a5135cdca6eddd049b358e6
fawad404 <fawadanxari31@gmail.com>
2024-08-11 03:54:14 +0500

    search-update

--- app/page.js
+++ app/page.js
@@ -146,31 +146,24 @@ export default function Home() {
   return (
     <>
       <Featured onSearch={handleSearch} />
-      {error ? (
-        <section className="relative py-20 md:py-32 overflow-hidden">
-        <div className="relative container px-4 mx-auto">
-          <div className="text-center mb-18">
+      {restaurants && (
+        <Product fetchedRestaurants={restaurants} />
+      )}
+      {error && (
+       
+        
+          <div className="text-center mb-18 -mt-18">
             <span className="inline-block py-1 px-3 mb-4 text-xs font-semibold text-orange-900 bg-orange-50 rounded-full"></span>
-            <h1 className="font-heading text-2xl xs:text-3xl md:text-4xl font-bold text-gray-900 mb-4">
+           {!restaurants && 
+            <h1 className="font-heading text-xl xs:text-2xl md:text-2xl font-bold text-gray-900 mb-4">
               <span>{error}</span>
             </h1>
+           }
             </div>
-            </div>
-            </section>
-      ) : restaurants ? (
-        <Product fetchedRestaurants={restaurants} />
-      ) : (
-        <section className="relative py-20 md:py-32 overflow-hidden">
-        <div className="relative container px-4 mx-auto">
-          <div className="text-center mb-18">
-            <span className="inline-block py-1 px-3 mb-4 text-xs font-semibold text-orange-900 bg-orange-50 rounded-full"></span>
-            <h1 className="font-heading text-2xl xs:text-3xl md:text-4xl font-bold text-gray-900 mb-4">
-              <span>Loading...</span>
-            </h1>
-            </div>
-            </div>
-            </section>
+            
+           
       )}
+      
       {data && (
         <div>
           {data.map((item) => (
